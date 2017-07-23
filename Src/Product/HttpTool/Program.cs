220bdb0faffe93b60b9b1cd0ca881b7968b2f690
James John McGuire <jamesjohnmcguire@gmail.com>
2017-07-23 11:38:59 +0000

    New web tester projects

---
+++ Src/Product/HttpTool/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebTools;
+
+namespace HttpTool
+{
+	internal class Program
+	{
+		static int Main(string[] args)
+		{
+			int returnCode = -1;
+
+			Program application = new Program();
+
+			bool resultCode = application.Run(args);
+
+			if (true == resultCode)
+			{
+				returnCode = 0;
+			}
+
+			return returnCode;
+		}
+
+		/////////////////////////////////////////////////////////////////////
+		// Run
+		/// <summary>
+		/// The main processing function
+		/// </summary>
+		/////////////////////////////////////////////////////////////////////
+		protected internal bool Run(string[] arguments)
+		{
+			bool successCode = false;
+
+			try
+			{
+				successCode = ValidateArguments(arguments);
+
+				if (false == successCode)
+				{
+					ShowUsage();
+				}
+				else
+				{
+					Console.WriteLine("Starting...");
+					SiteTest.Test(arguments[0]);
+				}
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception.ToString());
+			}
+
+			return successCode;
+		}
+
+		/////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// ShowUsage - Displays a message on how to use this application.
+		/// </summary>
+		/////////////////////////////////////////////////////////////////////
+		protected internal void ShowUsage()
+		{
+			Console.WriteLine("usage: HttpTool <URL>");
+		}
+
+		/////////////////////////////////////////////////////////////////////
+		/// <summary>
+		/// Summary for ValidateArguments.
+		/// </summary>
+		/////////////////////////////////////////////////////////////////////
+		protected internal bool ValidateArguments(string[] arguments)
+		{
+			bool result = false;
+
+			// Ensure we have a URL
+			if (arguments.Length > 0)
+			{
+				result = true;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ Src/Product/WebTools/SiteTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Abot.Core;
+using Abot.Crawler;
+using Abot.Poco;
+
+namespace WebTools
+{
+	public class SiteTest
+	{
+		public static void Test(string url)
+		{
+			//CrawlConfiguration crawlConfig =
+			//	AbotConfigurationSectionHandler.LoadFromXml().Convert();
+			PoliteWebCrawler crawler = new PoliteWebCrawler();
+			crawler.PageCrawlCompletedAsync += ProcessPageCrawlCompleted;
+
+			Uri uri = new Uri(url);
+			//This is synchronous, it will not go to the next line until the
+			//crawl has completed
+			CrawlResult result = crawler.Crawl(uri);
+
+			if (result.ErrorOccurred)
+			{
+				Console.WriteLine("Crawl of {0} completed with error: {1}",
+					result.RootUri.AbsoluteUri, result.ErrorException.Message);
+			}
+			else
+			{
+				Console.WriteLine("Crawl of {0} completed without error.",
+					result.RootUri.AbsoluteUri);
+			}
+		}
+
+		public static void ProcessPageCrawlCompleted(object sender,
+			PageCrawlCompletedArgs e)
+		{
+			CrawledPage crawledPage = e.CrawledPage;
+
+			if (crawledPage.WebException != null ||
+				crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+			}
+
+			Console.WriteLine("{0}: {1}",
+				crawledPage.HttpWebResponse.StatusCode,
+				crawledPage.Uri.AbsoluteUri);
+
+			Console.ForegroundColor = ConsoleColor.White;
+
+			string text = crawledPage.Content.Text;
+			if ((string.IsNullOrEmpty(text)) &&
+				(!crawledPage.Uri.AbsoluteUri.EndsWith(".jpg")))
+			{
+				Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+			}
+
+			//var htmlAgilityPackDocument = crawledPage.HtmlDocument; //Html Agility Pack parser
+			//var angleSharpHtmlDocument = crawledPage.AngleSharpHtmlDocument; //AngleSharp parser
+		}
+	}
+}
