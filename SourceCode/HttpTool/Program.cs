8060582730e86bb659df58569820de11681a90c7
JamesMc <jamesjohnmcguire@gmail.com>
2020-11-29 17:20:43 +0900

    Improvements based upon code analysis warnings. Copyright year updates. Added Resources.resx and StringTable. Ignore Resources.Designer file.

--- SourceCode/HttpTool/Program.cs
+++ SourceCode/HttpTool/Program.cs
@@ -6,19 +6,84 @@
 
 using CommandLine;
 using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
 using WebTools;
 
 namespace HttpTool
 {
 	internal class Program
 	{
+		private static readonly ResourceManager StringTable = new
+			ResourceManager(
+				"HttpTool.Resources",
+				Assembly.GetExecutingAssembly());
+
+		private static DocumentChecks GetTests(Parsed<object> parsed)
+		{
+			DocumentChecks tests = DocumentChecks.Basic;
+
+			switch (parsed.Value)
+			{
+				case Standard:
+					tests =
+						DocumentChecks.Basic | DocumentChecks.ContentErrors;
+					break;
+				case TestAll:
+					tests = DocumentChecks.Basic |
+						DocumentChecks.ContentErrors |
+						DocumentChecks.EmptyContent |
+						DocumentChecks.ImagesExist |
+						DocumentChecks.ParseErrors |
+						DocumentChecks.Redirect |
+						DocumentChecks.W3cValidation;
+					break;
+				case Enhanced:
+					tests = DocumentChecks.Basic |
+						DocumentChecks.ContentErrors |
+						DocumentChecks.EmptyContent |
+						DocumentChecks.ImagesExist |
+						DocumentChecks.ParseErrors |
+						DocumentChecks.Redirect;
+					break;
+				case AgilityPack:
+					tests = DocumentChecks.Basic |
+						DocumentChecks.ContentErrors |
+						DocumentChecks.ParseErrors;
+					break;
+				case Empty:
+					tests = DocumentChecks.Basic |
+						DocumentChecks.ContentErrors |
+						DocumentChecks.EmptyContent;
+					break;
+				case Images:
+					tests = DocumentChecks.Basic |
+						DocumentChecks.ContentErrors |
+						DocumentChecks.EmptyContent |
+						DocumentChecks.ImagesExist;
+					break;
+				case Redirects:
+					tests = DocumentChecks.Basic |
+						DocumentChecks.ContentErrors |
+						DocumentChecks.Redirect;
+					break;
+				case Validate:
+					tests = DocumentChecks.Basic |
+						DocumentChecks.ContentErrors |
+						DocumentChecks.EmptyContent |
+						DocumentChecks.W3cValidation;
+					break;
+			}
+
+			return tests;
+		}
+
 		private static int Main(string[] args)
 		{
 			int returnCode = -1;
 
-			Program application = new Program();
-
-			bool resultCode = application.Run(args);
+			bool resultCode = Run(args);
 
 			if (true == resultCode)
 			{
@@ -28,13 +93,21 @@ namespace HttpTool
 			return returnCode;
 		}
 
+		private static void Parsing(object options)
+		{
+		}
+
 		/////////////////////////////////////////////////////////////////////
 		// Run
 		/// <summary>
 		/// The main processing function.
 		/// </summary>
 		/////////////////////////////////////////////////////////////////////
-		private bool Run(string[] arguments)
+		[System.Diagnostics.CodeAnalysis.SuppressMessage(
+			"Style",
+			"IDE0017:Simplify object initialization",
+			Justification = "Don't agree with this rule.")]
+		private static bool Run(string[] arguments)
 		{
 			bool result = false;
 
@@ -61,105 +134,22 @@ namespace HttpTool
 					Options option = (Options)parsed.Value;
 					string url = option.Url.AbsoluteUri;
 
-					Console.WriteLine("Running tests on: {0}", url);
+					string message = StringTable.GetString(
+						"RUNNING_TESTS",
+						CultureInfo.InstalledUICulture);
+					Console.WriteLine(message, url);
 
 					tester.Test(url);
 				}
 			}
 			catch (Exception exception)
 			{
-				int code = (int)CommandLine.ParserResultType.NotParsed;
 				Console.WriteLine(exception.ToString());
-				Environment.Exit(code);
+
+				throw;
 			}
 
 			return result;
-		}
-
-		private static DocumentChecks GetTests(Parsed<object> parsed)
-		{
-			DocumentChecks tests = DocumentChecks.Basic;
-
-			switch (parsed.Value)
-			{
-				case Standard standard:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors;
-					break;
-				}
-
-				case TestAll testall:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors |
-						DocumentChecks.EmptyContent |
-						DocumentChecks.ImagesExist |
-						DocumentChecks.ParseErrors |
-						DocumentChecks.Redirect |
-						DocumentChecks.W3cValidation;
-					break;
-				}
-
-				case Enhanced enhanced:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors |
-						DocumentChecks.EmptyContent |
-						DocumentChecks.ImagesExist |
-						DocumentChecks.ParseErrors |
-						DocumentChecks.Redirect;
-					break;
-				}
-
-				case AgilityPack agilitypack:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors |
-						DocumentChecks.ParseErrors;
-					break;
-				}
-
-				case Empty empty:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors |
-						DocumentChecks.EmptyContent;
-					break;
-				}
-
-				case Images images:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors |
-						DocumentChecks.EmptyContent |
-						DocumentChecks.ImagesExist;
-					break;
-				}
-
-				case Redirects redirects:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors |
-						DocumentChecks.Redirect;
-					break;
-				}
-
-				case Validate validate:
-				{
-					tests = DocumentChecks.Basic |
-						DocumentChecks.ContentErrors |
-						DocumentChecks.EmptyContent |
-						DocumentChecks.W3cValidation;
-					break;
-				}
-			}
-
-			return tests;
-		}
-
-		private static void Parsing(object options)
-		{
 		}
 
 		/////////////////////////////////////////////////////////////////////
--- SourceCode/HttpTool/Properties/AssemblyInfo.cs
+++ SourceCode/HttpTool/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿/////////////////////////////////////////////////////////////////////////////
 // <copyright file="AssemblyInfo.cs" company="James John McGuire">
-// Copyright © 2016 - 2019 James John McGuire. All Rights Reserved.
+// Copyright © 2016 - 2020 James John McGuire. All Rights Reserved.
 // </copyright>
 /////////////////////////////////////////////////////////////////////////////
 
-using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Resources;
 using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -15,3 +14,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a09ee823-aefd-4fcd-a19a-8f04a20a1759")]
+
+[assembly: NeutralResourcesLanguage("en")]
