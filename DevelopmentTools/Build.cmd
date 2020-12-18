1eefbf45d8c4822f4f799a8bc9e35a9daf3c3783
JamesMc <jamesjohnmcguire@gmail.com>
2020-12-18 19:01:30 +0900

    Added release option to build script.

--- DevelopmentTools/Build.cmd
+++ DevelopmentTools/Build.cmd
@@ -1,10 +1,18 @@
 CD %~dp0
 CD ..
 
+IF EXIST Bin\Release\x64\NUL DEL /Q Bin\Release\x64\*.*
+
 dotnet build --configuration Release
 
+IF "%1"=="release" GOTO release
+GOTO end
+
+:release
 CD Bin\Release\x64
 
 7z u HttpTool.zip . -xr!*.json -xr!ref
 
-hub release create -a HttpTool.zip -m "%1" %1
+hub release create -a HttpTool.zip -m "%2" v%2
+
+:end
