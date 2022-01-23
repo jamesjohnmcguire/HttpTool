5b5efa1918298b82b7004b918c0d897cc096e161
James John McGuire <jamesjohnmcguire@gmail.com>
2022-01-23 20:38:43 +0900

    Publish cross platform applications

--- DevelopmentTools/Build.cmd
+++ DevelopmentTools/Build.cmd
@@ -1,9 +1,11 @@
 CD %~dp0
 CD ..\SourceCode
 
-IF EXIST Bin\Release\x64\NUL DEL /Q Bin\Release\x64\*.*
+IF EXIST Bin\Release\AnyCPU\NUL DEL /Q Bin\Release\AnyCPU\*.*
 
-dotnet build --configuration Release
+CALL dotnet publish -c Release --runtime linux-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\Linux HttpTool
+CALL dotnet publish -c Release --runtime osx-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\MacOS HttpTool
+CALL dotnet publish -c Release --runtime win-x64 --self-contained true -p:PublishReadyToRun=true -p:PublishSingleFile=true --output Binaries\Windows HttpTool
 
 IF "%1"=="release" GOTO release
 GOTO end
