a66d4848ec38497d57ddaf0d63aa1e210c7d3123
James John McGuire <jamesjohnmcguire@gmail.com>
2022-01-25 20:37:56 +0900

    Build file improvements

--- DevelopmentTools/Build.cmd
+++ DevelopmentTools/Build.cmd
@@ -3,9 +3,9 @@ CD ..\SourceCode
 
 IF EXIST Bin\Release\AnyCPU\NUL DEL /Q Bin\Release\AnyCPU\*.*
 
-CALL dotnet publish -c Release --runtime linux-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\Linux HttpTool
-CALL dotnet publish -c Release --runtime osx-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\MacOS HttpTool
-CALL dotnet publish -c Release --runtime win-x64 --self-contained true -p:PublishReadyToRun=true -p:PublishSingleFile=true --output Binaries\Windows HttpTool
+CALL dotnet publish --configuration Release --runtime linux-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\Linux HttpTool
+CALL dotnet publish --configuration Release --runtime osx-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\MacOS HttpTool
+CALL dotnet publish --configuration Release --runtime win-x64 --self-contained true -p:PublishReadyToRun=true -p:PublishSingleFile=true --output Binaries\Windows HttpTool
 
 IF "%1"=="release" GOTO release
 GOTO end
@@ -23,6 +23,7 @@ CD ..\Windows
 7z u HttpTool-Windows.zip .
 MOVE HttpTool-Windows.zip ..
 
-gh release create v%2 --notes "%3" *.zip
+CD ..
+gh release create v%2 --notes %3 *.zip
 
 :end
