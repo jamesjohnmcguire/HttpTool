f8c4ce8341b657e42ab539c0ec87d336f09aedcd
James John McGuire <jamesjohnmcguire@gmail.com>
2022-02-26 18:49:31 +0900

    Use consistent usage of command options

--- DevelopmentTools/Build.cmd
+++ DevelopmentTools/Build.cmd
@@ -3,9 +3,9 @@ CD ..\SourceCode
 
 IF EXIST Bin\Release\AnyCPU\NUL DEL /Q Bin\Release\AnyCPU\*.*
 
-CALL dotnet publish --configuration Release --runtime linux-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\Linux HttpTool
-CALL dotnet publish --configuration Release --runtime osx-x64 --self-contained true -p:PublishSingleFile=true -o Binaries\MacOS HttpTool
-CALL dotnet publish --configuration Release --runtime win-x64 --self-contained true -p:PublishReadyToRun=true -p:PublishSingleFile=true --output Binaries\Windows HttpTool
+CALL dotnet publish --configuration Release -p:PublishSingleFile=true --runtime linux-x64 --self-contained true --output Binaries\Linux HttpTool
+CALL dotnet publish --configuration Release -p:PublishSingleFile=true --runtime osx-x64 --self-contained true --output Binaries\MacOS HttpTool
+CALL dotnet publish --configuration Release -p:PublishReadyToRun=true;PublishSingleFile=true --runtime win-x64 --self-contained true --output Binaries\Windows HttpTool
 
 IF "%1"=="release" GOTO release
 GOTO end
