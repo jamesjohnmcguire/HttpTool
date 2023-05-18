8b51b3ef5eef186d22f8a211a3a45d90f8a46380
James John McGuire <jamesjohnmcguire@gmail.com>
2023-05-18 21:40:09 +0900

    Add First Test

--- SourceCode/WebTools.Test/Tests.cs
+++ SourceCode/WebTools.Test/Tests.cs
@@ -12,5 +12,18 @@ namespace WebTools.Test
 		{
 			Assert.Pass();
 		}
+
+		[Test]
+		public async Task TestHttpMinerBasic()
+		{
+			HttpClientExtended client = new HttpClientExtended();
+
+			Uri uri = new("https://www.digitalzenworks.com");
+			string response =
+				await client.Request(uri).ConfigureAwait(false);
+
+			Assert.NotNull(response);
+		}
+
 	}
 }
